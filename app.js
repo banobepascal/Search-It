44672c2fbfe7bcc8152a52b739ac8fe943336e0e
banobepascal <banobepascal@gmail.com>
2019-08-17 15:12:29 +0300

    finishing app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const search = document.querySelector('#inputBox');
-const matchList = document.querySelector('#result');
+const search = document.getElementById('inputBox');
+const matchList = document.getElementById('result');
 
 //search states.json and filter it
 const searchStates = async searchText => {
@@ -9,11 +9,12 @@ const searchStates = async searchText => {
     // get matches to current text input
     let matches = states.filter(state => {
         const regex = new RegExp(`^${searchText}`, 'gi');
-        return state.name.match(regex) || state.abbr.match(regex); 
+        return state.city.match(regex) || state.state.match(regex); 
     });
 
     if (searchText.length === 0) {
         matches = [];
+        matchList.innerHTML = '';
     }
 
     outputHtml(matches);
