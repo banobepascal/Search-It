7d73a1a33f57d561501b913ddd1793047533d041
banobepascal <banobepascal@gmail.com>
2019-08-17 14:14:51 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const search = document.querySelector('#search');
+const matchList = document.querySelector('#match-list');
+
+//search states.json and filter it
+const searchStates = async searchText => {
+    const res = await fetch(' ');
+    const states = await res.json();
+
+    // get matches to current text input
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex); 
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+    }
+
+    outputHtml(matches);
+};
+
+// show results in html
+const outputHtml = matches => {
+    if(matches.length > 0){
+      const html = matches.map(match => `
+       
+      `)  
+    }
+}
+
+search.addEventListener('input', () => searchStates(search.value));
